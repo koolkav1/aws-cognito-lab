ed488e7c932e2b4145f904a599c7588249466308
EC2 Default User <ec2-user@ip-172-31-17-206.eu-west-1.compute.internal>
2019-03-13 10:47:17 +0000

    updated signup function

--- js/app.js
+++ js/app.js
@@ -38,9 +38,96 @@
 	});
 
 	/***************** The main code ******************/
+	/***************** The main code ******************/
 
-	// Sign Up
-	function signUp(){
+	// Region must be defined
+	AWS.config.region = 'eu-west-1';
+
+	// User pool
+	var poolData = {
+			UserPoolId : 'eu-west-1_5oo05nVIq', // Your user pool id here
+			ClientId : '6opfdeu0qn8khv3kogu7o1s3ua' // Your app client id here
+	};
+
+	// Your identity pool id here
+	var identityPoolId = "eu-west-1:2d218a72-ae59-4bb9-872f-cc52408c7ea6"
+
+	// Cognito Sync store name
+	var cognitoDatasetName = "backspace-users";
+
+	var cognitoUser, identityId, cognitosync;
+
+		function signUp(){
+		console.log('Starting Sign up process');
+
+		// Close the modal window
+		$('#signUpModal').modal("hide");
+
+		// Get sign up information from modal
+		var userLogin = {
+			username : $('#inputPreferredUsername').val(),
+			password : $('#inputPassword').val()
+		}
+
+		var attributes = [
+			{
+				Name : 'given_name',
+				Value : $('#inputGivenName').val()
+			},
+			{
+					Name : 'family_name',
+					Value : $('#inputFamilyName').val()
+			},
+			{
+					Name : 'email',
+					Value : $('#inputEmail').val()
+			},
+			{
+					Name : 'preferred_username',
+					Value : $('#inputPreferredUsername').val()
+			},
+			{
+					Name : 'website',
+					Value : $('#inputWebsite').val()
+			},
+			{
+					Name : 'gender',
+					Value : $('#inputGender').val()
+			},
+			{
+					Name : 'birthdate',
+					Value : $('#inputBirthdate').val()
+			},
+			{
+					Name : 'custom:linkedin',
+					Value : $('#inputLinkedin').val()
+			}
+		];
+
+		var params = {
+		  ClientId: poolData.ClientId, 	/* required */
+		  Password: userLogin.password, /* required */
+		  Username: userLogin.username, /* required */
+		  ValidationData: [],						/* required */
+		  UserAttributes: attributes
+		};
+
+		var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+		cognitoidentityserviceprovider.signUp(params, function(err, data) {
+		  if (err) {
+				console.log(err, err.stack); // an error occurred
+				alert('Error: '+ JSON.stringify(err));
+			}
+		  else {
+				console.log(JSON.stringify(data));           // successful response
+				if (data.UserConfirmed) {
+					bootbox.alert('Sign up successful.');
+				}
+				else{
+					bootbox.alert('Please check your email for a verification link.');
+				}
+			}
+		});
 	}
 
 	// Sign In
